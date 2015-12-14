43e8145dbfaebcc546ede4e5c70b0b5c842e32d8
SMCCOL202 <smccol202@caledonian.ac.uk>
2015-12-14 02:16:17 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,313 @@
+#define WIN32_LEAN_AND_MEAN
+#define WIN32_EXTRA_LEAN
+
+#define GLX_GLXEXT_LEGACY //Must be declared so that our local glxext.h is picked up, rather than the system one
+
+
+#include <windows.h>
+#include "windowOGL.h"
+#include "GameConstants.h"
+#include "cWNDManager.h"
+#include "cInputMgr.h"
+#include "cSoundMgr.h"
+#include "cFontMgr.h"
+#include "cSprite.h"
+#include "asteroidsGame.h"
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR cmdLine,
+                   int cmdShow)
+{
+
+    //Set our window settings
+    const int windowWidth = 1024;
+    const int windowHeight = 768;
+    const int windowBPP = 16;
+
+
+
+    //This is our window
+	static cWNDManager* pgmWNDMgr = cWNDManager::getInstance();
+
+	// This is the input manager
+	static cInputMgr* theInputMgr = cInputMgr::getInstance();
+
+	// This is the sound manager
+	static cSoundMgr* theSoundMgr = cSoundMgr::getInstance();
+
+	// This is the Font manager
+	static cFontMgr* theFontMgr = cFontMgr::getInstance();
+
+	//The example OpenGL code
+    windowOGL theOGLWnd;
+	
+    //Attach our the OpenGL window
+	pgmWNDMgr->attachOGLWnd(&theOGLWnd);
+
+	// Attach the keyboard manager
+	pgmWNDMgr->attachInputMgr(theInputMgr);
+
+    //Attempt to create the window
+	if (!pgmWNDMgr->createWND(windowWidth, windowHeight, windowBPP))
+    {
+        //If it fails
+
+        MessageBox(NULL, "Unable to create the OpenGL Window", "An error occurred", MB_ICONERROR | MB_OK);
+		pgmWNDMgr->destroyWND(); //Reset the display and exit
+        return 1;
+    }
+
+	if (!theOGLWnd.initOGL(windowWidth, windowHeight)) //Initialize our example
+    {
+        MessageBox(NULL, "Could not initialize the application", "An error occurred", MB_ICONERROR | MB_OK);
+		pgmWNDMgr->destroyWND(); //Reset the display and exit
+        return 1;
+    }
+
+	//Clear key buffers
+	theInputMgr->clearBuffers(theInputMgr->KEYS_DOWN_BUFFER | theInputMgr->KEYS_PRESSED_BUFFER);
+
+	/* initialize random seed: */
+	srand((unsigned int)time(NULL));
+
+	// Create vector array of textures
+	LPCSTR texturesToUse[] = { "Images\\space-invaders1.png", "Images\\space-invaders2.png", "Images\\space-invaders3.png", "Images\\space-invaders4.png", "Images\\Bullet2.png" };
+	for (int tCount = 0; tCount < 5; tCount++)
+	{
+		theGameTextures.push_back(new cTexture());
+		theGameTextures[tCount]->createTexture(texturesToUse[tCount]);
+	}
+
+	// load game sounds
+	// Load Sound
+	LPCSTR gameSounds[3] = { "Audio/Space_Coast.mp3", "Audio/shoot_sound.wav", "Audio/enemy_death.wav" };
+
+	theSoundMgr->add("Theme", gameSounds[0]);
+	theSoundMgr->add("Shot", gameSounds[1]);
+	theSoundMgr->add("Explosion", gameSounds[2]);
+
+	// load game fontss
+	// Load Fonts
+	LPCSTR gameFonts[2] = { "Fonts/digital-7.ttf", "Fonts/space age.ttf" };
+
+	theFontMgr->addFont("SevenSeg", gameFonts[0], 24);
+	theFontMgr->addFont("Space", gameFonts[1], 24);
+
+	// Create vector array of textures
+	vector<cTexture*> textureBkgList;
+	LPCSTR bkgTexturesToUse[] = { "Images/space_invaders-wallpaper-1024x768.png", "Images/Start_Screen.png", "Images/Game_Over_Screen.png" };
+	for (int tCount = 0; tCount < 3; tCount++)
+	{
+		textureBkgList.push_back(new cTexture());
+		textureBkgList[tCount]->createTexture(bkgTexturesToUse[tCount]);
+	}
+
+	//A blank texture
+	cTexture transSprite;
+	transSprite.createTexture("Images/blank.png");
+
+	//Background texture for main game screen
+	cBkGround spriteBkgd;
+	spriteBkgd.setSpritePos(glm::vec2(0.0f, 0.0f));
+	spriteBkgd.setTexture(textureBkgList[0]->getTexture());
+	spriteBkgd.setTextureDimensions(textureBkgList[0]->getTWidth(), textureBkgList[0]->getTHeight());
+
+	//Background texture for start screen
+	cBkGround spriteStartBkgd;
+	spriteStartBkgd.setSpritePos(glm::vec2(0.0f, 0.0f));
+	spriteStartBkgd.setTexture(textureBkgList[1]->getTexture());
+	spriteStartBkgd.setTextureDimensions(textureBkgList[1]->getTWidth(), textureBkgList[1]->getTHeight());
+
+	//Background texture for game over screen
+	cBkGround spriteEndBkgd;
+	spriteEndBkgd.setSpritePos(glm::vec2(0.0f, 0.0f));
+	spriteEndBkgd.setTexture(textureBkgList[2]->getTexture());
+	spriteEndBkgd.setTextureDimensions(textureBkgList[2]->getTWidth(), textureBkgList[2]->getTHeight());
+
+	// Create vector array of button textures
+	vector<cTexture*> btnTextureList;
+	LPCSTR btnTexturesToUse[] = { "Images/Buttons/exitBtn.png", "Images/Buttons/instructionsBtn.png", "Images/Buttons/loadBtn.png", "Images/Buttons/playBtn.png", "Images/Buttons/saveBtn.png", "Images/Buttons/settingsBtn.png" };
+	for (int tCount = 0; tCount < 6; tCount++)
+	{
+		btnTextureList.push_back(new cTexture());
+		btnTextureList[tCount]->createTexture(btnTexturesToUse[tCount]);
+	}
+
+	//Uses for loop to create 5 space invaders and place them randomly around the game screen
+	//Also sets their velocity, hit-box, and texture
+	for (int astro = 0; astro < 5; astro++)
+	{
+		theAsteroids.push_back(new cAsteroid);
+		theAsteroids[astro]->setSpritePos(glm::vec2(windowWidth / (rand() % 5 + 1), windowHeight / (rand() % 5 + 1)));
+		theAsteroids[astro]->setSpriteTranslation(glm::vec2((rand() % 4 + 1), (rand() % 4 + 1)));
+		int randAsteroid = rand() % 4;
+		theAsteroids[astro]->setTexture(theGameTextures[randAsteroid]->getTexture());
+		theAsteroids[astro]->setTextureDimensions(theGameTextures[randAsteroid]->getTWidth(), theGameTextures[randAsteroid]->getTHeight());
+		theAsteroids[astro]->setSpriteCentre();
+		theAsteroids[astro]->setAsteroidVelocity(glm::vec2(glm::vec2(0.0f, 0.0f)));
+		theAsteroids[astro]->setActive(true);
+		theAsteroids[astro]->setMdlRadius();
+	}
+
+	//Creates texture for Ship sprite, as well as the controls for it and the hit-box
+	//We don't need this as a list as there is only one ship, and it is only ever used once
+	cTexture rocketTxt;
+	rocketTxt.createTexture("Images\\ship.png");
+	cRocket rocketSprite;
+	rocketSprite.attachInputMgr(theInputMgr); // Attach the input manager to the sprite
+	rocketSprite.setSpritePos(glm::vec2(512.0f, 380.0f));
+	rocketSprite.setTexture(rocketTxt.getTexture());
+	rocketSprite.setTextureDimensions(rocketTxt.getTWidth(), rocketTxt.getTHeight());
+	rocketSprite.setSpriteCentre();
+	rocketSprite.setRocketVelocity(glm::vec2(0.0f, 0.0f));
+
+	// Attach sound manager to rocket sprite
+	rocketSprite.attachSoundMgr(theSoundMgr);
+
+
+	gameState theGameState = MENU;
+	btnTypes theBtnType = EXIT;
+
+    //This is the mainloop, we render frames until isRunning returns false
+	while (pgmWNDMgr->isWNDRunning())
+    {
+		pgmWNDMgr->processWNDEvents(); //Process any window events
+
+        //We get the time that passed since the last frame
+		float elapsedTime = pgmWNDMgr->getElapsedSeconds();
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		/* Attempted to get this working, but simply too many nonsensical errors popped up
+		For me to even comprehend how to get it running.
+
+		//Used to 'describe' how each scene should be.
+		switch (theGameState)
+		{
+
+		//The main menu scene
+		case MENU:
+			spriteStartBkgd.render();
+
+						playButton.setSpritePos(glm::vec2(400.0f, 300.0f));
+			exitButton.setSpritePos(glm::vec2(400.0f, 375.0f));
+			playButton.render();
+			exitButton.render();
+
+			theGameState = playButton.update(theGameState, PLAYING);
+			exitButton.update();
+
+
+			outputMsg = strMsg[2];
+			theFontMgr->getFont("Space")->printText(outputMsg.c_str(), FTPoint(10, 15, 0.0f));
+			outputMsg = strMsg[0];
+			theFontMgr->getFont("Space")->printText(outputMsg.c_str(), FTPoint(100, 100, 0.0f));
+			outputMsg = strMsg[1];
+			theFontMgr->getFont("Space")->printText(outputMsg.c_str(), FTPoint(100, 150, 0.0f));
+			if (exitButton.getClicked())
+			{
+				SendMessage(pgmWNDMgr->getWNDHandle(), WM_CLOSE, NULL, NULL);
+			}
+			break;
+
+			//The actual game screen
+		case PLAYING:
+			spriteBkgd.render();
+
+		rocketSprite.update(elapsedTime);
+
+		vector<cAsteroid*>::iterator asteroidIterator = theAsteroids.begin();
+		while (asteroidIterator != theAsteroids.end())
+		{
+			if ((*asteroidIterator)->isActive() == false)
+			{
+				asteroidIterator = theAsteroids.erase(asteroidIterator);
+			}
+			else
+			{
+				(*asteroidIterator)->update(elapsedTime);
+				(*asteroidIterator)->render();
+				++asteroidIterator;
+			}
+		}
+
+			// render button and reset clicked to false
+			saveButton.setSpritePos(glm::vec2(740.0f, 500.0f));
+			saveButton.render();
+			saveButton.update("Data/usermap.dat", tileMap.getMap());
+
+			exitButton.setSpritePos(glm::vec2(740.0f, 575.0f));
+			exitButton.render();
+			theGameState = exitButton.update(theGameState, END);
+
+			outputMsg = strMsg[2];
+			theFontMgr->getFont("Space")->printText(outputMsg.c_str(), FTPoint(10, 15, 0.0f));
+
+			break;
+
+			//The game over screen
+		case END:
+			spriteEndBkgd.render();
+
+			playButton.setClicked(false);
+			exitButton.setClicked(false);
+
+			playButton.setSpritePos(glm::vec2(400.0f, 300.0f));
+			exitButton.setSpritePos(glm::vec2(400.0f, 375.0f));
+			playButton.render();
+			exitButton.render();
+
+			theGameState = playButton.update(theGameState, PLAYING);
+			exitButton.update();
+
+			outputMsg = strMsg[2];
+			theFontMgr->getFont("Space")->printText(outputMsg.c_str(), FTPoint(10, 15, 0.0f));
+			outputMsg = strMsg[3];
+			theFontMgr->getFont("Space")->printText(outputMsg.c_str(), FTPoint(100, 100, 0.0f));
+			outputMsg = strMsg[4];
+			theFontMgr->getFont("Space")->printText(outputMsg.c_str(), FTPoint(100, 150, 0.0f));
+			if (exitButton.getClicked())
+			{
+				SendMessage(pgmWNDMgr->getWNDHandle(), WM_CLOSE, NULL, NULL);
+			}
+
+			break;
+		}
+
+
+		}
+		*/
+		
+		
+		spriteBkgd.render();
+
+		rocketSprite.update(elapsedTime);
+
+		vector<cAsteroid*>::iterator asteroidIterator = theAsteroids.begin();
+		while (asteroidIterator != theAsteroids.end())
+		{
+			if ((*asteroidIterator)->isActive() == false)
+			{
+				asteroidIterator = theAsteroids.erase(asteroidIterator);
+			}
+			else
+			{
+				(*asteroidIterator)->update(elapsedTime);
+				(*asteroidIterator)->render();
+				++asteroidIterator;
+			}
+		}
+
+		rocketSprite.render();
+		theFontMgr->getFont("Space")->printText("Space Invaders", FTPoint(0.0f, -1.0f, 0.0f));
+
+		pgmWNDMgr->swapBuffers();
+		theInputMgr->clearBuffers(theInputMgr->KEYS_DOWN_BUFFER | theInputMgr->KEYS_PRESSED_BUFFER);
+    }
+
+	theOGLWnd.shutdown(); //Free any resources
+	pgmWNDMgr->destroyWND(); //Destroy the program window
+
+    return 0; //Return success
+}
