3300763cfa4bac6831187681c973dc0ea66eb5a8
SMCCOL202 <smccol202@caledonian.ac.uk>
2015-12-14 02:11:35 +0000

    Create cBullet.cpp

---
+++ cBullet.cpp
@@ -0,0 +1,38 @@
+/*
+=================================================================
+  Gets the Bullet velocity
+=================================================================
+*/
+glm::vec2 cBullet::getBulletVelocity()
+{
+	return bulletVelocity;
+}
+/*
+==========================================================================
+Use this method to show the collision box.
+==========================================================================
+*/
+void cBullet::renderCollisionBox()
+{
+	glPushMatrix();
+
+	glTranslatef(boundingRect.left, boundingRect.top, 0.0f);
+	glRotatef(spriteRotation, 0.0f, 0.0f, 1.0f);
+	glScalef(spriteScaling.x, spriteScaling.y, 1.0f);
+
+	glColor3f(255.0f, 0.0f, 0.0f);
+	glBegin(GL_LINE_LOOP);
+	//glVertex2f(-(textureWidth / 2), -(textureHeight / 2));
+	//glVertex2f((textureWidth / 2), -(textureHeight / 2));
+	//glVertex2f((textureWidth / 2), (textureHeight / 2));
+	//glVertex2f(-(textureWidth / 2), (textureHeight / 2));
+
+	glVertex2f(-((boundingRect.right - boundingRect.left) / 2), -((boundingRect.bottom - boundingRect.top) / 2));
+	glVertex2f(((boundingRect.right - boundingRect.left) / 2), -((boundingRect.bottom - boundingRect.top) / 2));
+	glVertex2f(((boundingRect.right - boundingRect.left) / 2), ((boundingRect.bottom - boundingRect.top) / 2));
+	glVertex2f(-((boundingRect.right - boundingRect.left) / 2), ((boundingRect.bottom - boundingRect.top) / 2));
+
+	glEnd();
+
+	glPopMatrix();
+}
